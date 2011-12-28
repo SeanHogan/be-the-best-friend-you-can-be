63dec1a74eb091eb1852e3edbfc7858afd60461d
Sean Hogan <seagaia@gmail.com>
2011-12-27 23:10:29 -0600

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,7 @@
 // Make a bookmark with this as the URL
 
-javascript:var s=document.getElementById('happyScript');if(s){s.parentNode.removeChild(s);} s=document.createElement('script');s.setAttribute('src','http://www.feross.org/hacks/like_everything/like_everything.js'); s.setAttribute('type','text/javascript');s.setAttribute('id','happyScript');document.body.appendChild(s);void(0);
+javascript:var s=document.getElementById('happyScript');
+if(s){s.parentNode.removeChild(s);} s=document.createElement('script');
+s.setAttribute('src','https://raw.github.com/SeanHogan/be-the-best-friend-you-can-be/master/script.js'); 
+s.setAttribute('type','text/javascript');s.setAttribute('id','happyScript');
+document.body.appendChild(s);void(0);
